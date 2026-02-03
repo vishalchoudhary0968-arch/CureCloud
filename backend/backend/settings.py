cf4d607f898627e55762f972d3c32dca59fa33e8
Vishal Choudhary <vishalchoudhary0968@gmail.com>
2026-02-03 21:57:08 +0530

    initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,14 +81,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CureCloud',
-        'USER': 'root',
-        'PASSWORD': 'satyamrana',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
